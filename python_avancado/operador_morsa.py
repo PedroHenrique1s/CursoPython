e23464cae4817f19820fba820cc86921bafeed43
pedro.souza <pedrosouza.info@mazatarraf.com.br>
2025-02-07 10:09:19 -0300

    feat(operador_morsa)- conceito do operador morsa

---
+++ python_avancado/operador_morsa.py
@@ -0,0 +1,29 @@
+# Operador Morsa 
+
+# valor_de_busca = 'xxx'
+
+# resultado = buscar_no_banco_de_dados(valor_de_busca)
+
+# if resultado is None:
+#     print(f'Nada foi encontrado para o valor de busca "{valor_de_busca}"')
+# else:
+#     print(f'Resultados encontrados para valor de busca "{valor_de_busca}": {resultado}')
+
+
+# valor_de_busca = 'xxx'
+
+# if (resultado :=  buscar_no_banco_de_dados(valor_de_busca)) is None:
+#     print(f'Nada foi encontrado para o valor de busca "{valor_de_busca}"')
+# else:
+#     print(f'Resultados encontrados para valor de busca "{valor_de_busca}": {resultado}')
+
+
+# n = 5
+# while n > 5:
+#     n -= 1
+#     print(n)
+
+
+# n = 5
+# while (n := n-1) > 0:
+#     print(n)
